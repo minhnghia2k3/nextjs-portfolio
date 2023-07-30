3f28d91eb25439a264dae23cb58988390713749e
LE MINH NGHIA <BrozennVN@gmail.com>
2023-07-30 11:35:16 +0700

    Add section contact & custom scrollbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Email from '@/components/Email';
 import SocialIcons from '@/components/SocialIcons';
 import About from '@/sections/About';
+import Contact from '@/sections/Contact';
 import Experience from '@/sections/Experience';
 import Hero from '@/sections/Hero';
 import Navbar from '@/sections/Navbar';
@@ -19,6 +20,7 @@ function Index() {
         <About />
         <Experience />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
